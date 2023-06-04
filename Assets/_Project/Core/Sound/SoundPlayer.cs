6e0eea266239cd9466d739a276aef35d219b9205
Pavlo Panko <pavlikosar@gmail.com>
2023-06-04 12:00:47 +0300

    Update ball sound

---
+++ Assets/_Project/Core/Sound/SoundPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Core
+{
+    [RequireComponent (typeof (AudioSource))]
+    public class SoundPlayer : MonoBehaviour
+    {
+        private AudioSource _audioSource;
+
+        public void Play(AudioClip sound) => _audioSource.PlayOneShot(sound);
+
+        public void Play(List<AudioClip> sounds) => Play(sounds[Random.Range(0, sounds.Count)]);
+
+        private void Awake() =>_audioSource = GetComponent<AudioSource>();
+    }
+}

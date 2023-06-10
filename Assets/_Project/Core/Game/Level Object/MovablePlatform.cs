d41e7cdbe8509dfe4b12ca3e3a553a1fcb0ae02d
Pavlo Panko <pavlikosar@gmail.com>
2023-06-10 19:49:09 +0300

    Add movable platform

---
+++ Assets/_Project/Core/Game/Level Object/MovablePlatform.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Core
+{
+    public class MovablePlatform : MonoBehaviour
+    {
+        [SerializeField] private Vector2 _start;
+        [SerializeField] private Vector2 _end;
+
+        [Space]
+        [SerializeField] private float _speed = 1;
+        [SerializeField] private MovablePlatformDirection _startDirection = MovablePlatformDirection.Right;
+
+        private Transform _transform;
+
+        private void Start()
+        {
+            _transform = transform;
+            StartCoroutine(StartMove());
+        }
+
+        private IEnumerator StartMove()
+        {
+            int direction = (int)_startDirection;
+            float time = 0;
+            _transform.position = _start;
+
+            if (direction == -1)
+            {
+                time = 1;
+                _transform.position = _end;
+            }
+
+            while(true) 
+            {
+                time += Time.deltaTime * direction * _speed;
+
+                if (time >= 1)
+                {
+                    direction = -1;
+                    time = 1;
+                }
+                else if (time <= 0)
+                {
+                    direction = 1;
+                    time = 0;
+                }
+
+                _transform.localPosition = Vector2.Lerp(_start, _end, time);
+
+                yield return new WaitForEndOfFrame();
+            }
+        }
+    }
+
+    public enum MovablePlatformDirection
+    {
+        Left = -1,
+        Right = 1
+    }
+}
